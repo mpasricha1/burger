4830925eeef8efc33a2924debc212b18d4f8af78
Mark Pasricha <markp862@gmail.com>
2021-03-15 21:09:08 -0400

    added .env file

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql"); 
+require("dotenv").config(); 
 
 const connection = mysql.createConnection({
-	host: "localhost", 
+	host: process.env.DB_HOST, 
 	port: 3306,
-	user: "root",
-	password: "root", 
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS, 
 	database: "burgers_db"
 }); 
 
