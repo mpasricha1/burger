d2e1af4466898a99952bca0ef9b014a114d0da84
Mark Pasricha <markp862@gmail.com>
2021-03-20 11:41:33 -0400

    fixing database

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,17 @@
 const mysql = require("mysql"); 
 require("dotenv").config(); 
+let connection; 
 
-const connection = mysql.createConnection({
-	host: process.env.DB_HOST, 
-	port: process.env.DB_PORT,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS, 
-	database: process.env.DB
-}); 
-
-connection.connect( (err) => {
-	if(err) { 
-		console.error(`error connecting: ${err.stack}`); 
-		return;
-	}
-	console.log("connected as id " + connection.threadId);
-}); 
-
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL); 
+}else{
+	connection = mysql.createConnection({
+		host: process.env.DB_HOST, 
+		port: process.env.DB_PORT,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASS, 
+		database: process.env.DB
+	}); 
+};
+connection.connect();
 module.exports = connection;
